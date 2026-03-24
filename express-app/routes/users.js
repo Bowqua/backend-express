cb0e113fc1193384330722129219c3efa31b40e4
Quatov <nooasarano21@gmail.com>
2026-03-24 19:02:17 +0500

    Added /GET/users/:id

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -15,4 +15,19 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:id', function(req, res, next) {
+    const id = Number(req.params.id);
+    const user = users.find(function (user) {
+        return user.id === id;
+    });
+
+    if (!user) {
+        return res.status(404).json({
+            error: 'User not found'
+        });
+    }
+
+    res.json(user);
+});
+
 module.exports = router;
