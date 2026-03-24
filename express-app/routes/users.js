fc78312d7a1236abca8182ed4e51616006488b37
Quatov <nooasarano21@gmail.com>
2026-03-24 18:43:06 +0500

    new json for users

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
+
+const users = [
+    { id: 1, name: 'Jonathan' },
+    { id: 2, name: 'Sarah' },
+    { id: 3, name: 'John' }
+];
+
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.json({
+      items: users
+  });
 });
 
 module.exports = router;
